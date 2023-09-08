d05952a49b22cdc06b8f5bf14c4e08424a0a16f7
Tugcem Yalcin <tugcemyalcin@gmail.com>
2023-09-08 15:11:55 +1000

    Using async/await instead of promises.

--- services/passport.js
+++ services/passport.js
@@ -20,15 +20,14 @@ passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    const user = User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+}, 
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+
+    if (existingUser) {
+      done(null, existingUser);
+    }
+
+    const user = await new User({ googleId: profile.id }).save()
+    done(null, user);
 }));
